b006dd44510cd80b4887d2241ac67a4444d72677
valhayot <valeriehayot@gmail.com>
2025-08-15 11:02:39 -0400

    Add passthrough function registration script for viewing

---
+++ __tests__/gc_function.py
@@ -0,0 +1,11 @@
+from globus_compute_sdk import Client
+from globus_compute_sdk.serialize import JSONData
+
+
+def passthrough(*args, **kwargs):
+    return kwargs
+
+
+gcc = Client(data_serialization_strategy=JSONData())
+function_uuid = gcc.register_function(passthrough, public=True)
+print(f"Registered passthrough function UUID: {function_uuid}")
