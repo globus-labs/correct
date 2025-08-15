ade2d93de00e1368d0b0fc739dc2e13bd327aa6a
valhayot <valeriehayot@gmail.com>
2025-08-15 10:53:18 -0400

    Change function UUID to a publicly accessible one

--- src/main.ts
+++ src/main.ts
@@ -78,6 +78,7 @@ export async function run(): Promise<void> {
     console.log(`Cloning repo ${url}`)
     const cmd = `if [ -d ${tmp_workdir} ]; then rm -r ${tmp_workdir}; fi && mkdir ${tmp_workdir} && cd ${tmp_workdir} && git clone ${url} && cd ${repo.repo} && git checkout ${branch}`
     console.log('Registering function')
+    console.log(`${CLIENT_ID} ${CLIENT_SECRET}`)
     const clone_reg = await register_function(access_token, cmd)
     const clone_uuid = clone_reg.function_uuid
 
