d1e7b8bb4c2e8329d52d1d7e0fc52c3902dd7d8d
valhayot <valeriehayot@gmail.com>
2025-11-07 18:14:04 -0500

    [WIP] fix identity env var issue

--- src/pysrc/shellfunction.py
+++ src/pysrc/shellfunction.py
@@ -288,8 +288,8 @@ def main():
     cmd = sys.argv[2]
     endpoint_config = json.loads(sys.argv[3])
 
-    client_id = os.getenv("secrets.GLOBUS_CLI_CLIENT_ID")
-    client_secret = os.getenv("secrets.GLOBUS_CLI_CLIENT_SECRET")
+    client_id = os.getenv("GLOBUS_COMPUTE_CLIENT_ID")
+    client_secret = os.getenv("GLOBUS_COMPUTE_CLIENT_SECRET")
 
     print(f"Running command: {cmd}", file=sys.stderr)
 
